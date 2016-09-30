6e15c441c610eb9f1244ee8d60a614aa6f286cb2
Marcel Good <marcelg@ideablade.com>
2016-09-29 21:02:28 -0700

    Fix `npm run entities` failing

--- TempHire/tools/tsgen-core.js
+++ TempHire/tools/tsgen-core.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
 var path = require('path');
-var breeze = require('breeze-client');
+var breeze = require('breeze-client/breeze.debug.js');
 var handlebars = require('handlebars');
 var _ = require('lodash');
 
